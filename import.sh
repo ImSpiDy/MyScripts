3f8129c73d00a4902c17dedc5b9936ba7cf29526
ImSpiDy <SpiDy2713@gmail.com>
2022-06-05 07:22:37 +0530

    import.sh: update fetch link
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- import.sh
+++ import.sh
@@ -9,25 +9,25 @@ read -p "Please input the tag name: " tag
 
 read -p "Import qcacld-3.0, qca-wifi-host-cmn, fw-api? ( y or n ): "  ans
 if [ $ans = "y" ]; then
-git subtree add --prefix=drivers/staging/qcacld-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $tag
+git subtree add --prefix=drivers/staging/qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $tag
 
-git subtree add --prefix=drivers/staging/qca-wifi-host-cmn https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $tag
+git subtree add --prefix=drivers/staging/qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $tag
 
-git subtree add --prefix=drivers/staging/fw-api https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $tag
+git subtree add --prefix=drivers/staging/fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api $tag
 else
 echo " Skipped qcacld-3.0, qca-wifi-host-cmn, fw-api"
 fi
 
 read -p " Wanna import audio-kernel? ( y or n ): " ans1
 if [ $ans1 = "y" ]; then
-git subtree add --prefix=techpack/audio https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $tag
+git subtree add --prefix=techpack/audio https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel/ $tag
 else
 echo " Skipped audio-kernel"
 fi
 
 read -p " Wanna import data-kernel? ( y or n ): " ans2
 if [ $ans2 = "y" ]; then
-git subtree add --prefix=techpack/data https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel $tag
+git subtree add --prefix=techpack/data https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/data-kernel $tag
 else
 echo " Skipped data-kernel"
 fi
