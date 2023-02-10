9b7febe98e3ded042a583c93343514d2c6efad47
ImSpiDy <SpiDy2713@gmail.com>
2023-02-10 17:05:55 +0530

    import: improve update script
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- import.sh
+++ import.sh
@@ -4,24 +4,37 @@
 #
 
 kt_dir="$(pwd)"
+Link=https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan
 
 read -p "Please input the tag name: " tag
 
-read -p "Import qcacld-3.0, qca-wifi-host-cmn, fw-api? ( y or n ): "  ans
+read -p "Wanna Import or update qcacld-3.0, qca-wifi-host-cmn, fw-api? ( y or n ): "  ans
 if [ $ans = "y" ]; then
-git subtree add --prefix=drivers/staging/qcacld-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $tag
+if [ -d drivers/staging/qcacld-3.0 ]; then
+git subtree pull --prefix=drivers/staging/qcacld-3.0 $Link/qcacld-3.0 $tag
+else
+git subtree add --prefix=drivers/staging/qcacld-3.0 $Link/qcacld-3.0 $tag
+fi
 
-git subtree add --prefix=drivers/staging/qca-wifi-host-cmn https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $tag
+if [ -d drivers/staging/qca-wifi-host-cmn ]; then
+git subtree pull --prefix=drivers/staging/qca-wifi-host-cmn $Link/qca-wifi-host-cmn $tag
+else
+git subtree add --prefix=drivers/staging/qca-wifi-host-cmn $Link/qca-wifi-host-cmn $tag
+fi
 
-git subtree add --prefix=drivers/staging/fw-api https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $tag
+if [ -d drivers/staging/fw-api ]; then
+git subtree pull --prefix=drivers/staging/fw-api $Link/fw-api $tag
+else
+git subtree add --prefix=drivers/staging/fw-api $Link/fw-api $tag
+fi
 else
 echo " Skipped qcacld-3.0, qca-wifi-host-cmn, fw-api"
 fi
 
-read -p " Wanna import Exfat? ( y or n ): " ans3
-if [ $ans3 = "y" ]; then
-read -p " Linux Version is 4.4 or Below 4.4 ? ( y or n ): " ans4
-if [ $ans4 = "y" ]; then
+read -p " Wanna import Exfat? ( y or n ): " ans1
+if [ $ans1 = "y" ]; then
+read -p " Linux Version is 4.4 or Below 4.4 ? ( y or n ): " ans2
+if [ $ans2 = "y" ]; then
 git subtree add --prefix=fs/exfat https://github.com/arter97/exfat-linux old
 else
 git subtree add --prefix=fs/exfat https://github.com/arter97/exfat-linux master
