f7a203221958b2aa3442b1c102b419e5243df61e
ImSpiDy <SpiDy2713@gmail.com>
2023-07-11 16:06:14 +0530

    CI: Bringup for LeafOS 2.0
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- buildOS.sh
+++ buildOS.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
+mkdir -p leaf
+
 pkg () {
 apt install -y lld git-lfs
 }
 
 sync () {
-# Sync source
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 11.0 --git-lfs
+cd /leaf
+
+repo init --depth=1 --no-repo-verify -u https://github.com/LeafOS-Project/android.git -b leaf-2.0 -g default,-mips,-darwin,-notdefault
 
 # Sync source without unnecessary messages, try with -j30 first, if fails, it will try again
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
 
-# tree
-git clone --depth=1 https://github.com/ImSpiDy/device_xiaomi_lavender device/xiaomi/lavender -b R
-git clone --depth=1 https://github.com/ImSpiDy/vendor_xiaomi_lavender vendor/xiaomi/lavender -b R
-git clone --depth=1 https://github.com/ImSpiDy/kernel_xiaomi_lavender kernel/xiaomi/lavender -b X5.1
+# device stuff
+git clone --depth=1 https://github.com/ImSpiDy/device_xiaomi_lavender device/xiaomi/lavender -b A13
+git clone --depth=1 https://github.com/ImSpiDy/vendor_xiaomi_lavender vendor/xiaomi/lavender -b TF
+git clone --depth=1 https://github.com/ImSpiDy/kernel_xiaomi_lavender kernel/xiaomi/lavender -b X7-Qti
 
-# extra stuff
-#git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_livedisplay hardware/lineage/livedisplay -b lineage-17.1
-#git clone --depth=1 https://github.com/Nub-XD/android_packages_apps_Dirac packages/apps/Dirac -b 10
-#git clone --depth=1 https://github.com/Nub-XD/platform_vendor_lawnchair vendor/lawnchair -b 10
-git clone --depth=1 https://gitlab.com/Project-Nexus/nexus-clang.git prebuilts/clang/host/linux-x86/nexus-clang -b nexus-14
+#hals
+rm -rf hardware/qcom-caf/msm8998/audio hardware/qcom-caf/msm8998/media hardware/qcom-caf/msm8998/display
+git clone --depth=1 https://github.com/wHo-EM-i/android_hardware_qcom_audio hardware/qcom-caf/msm8998/audio -b 8998-4.4
+git clone --depth=1 https://github.com/wHo-EM-i/android_hardware_qcom_display hardware/qcom-caf/msm8998/display -b 8998-4.4
+git clone --depth=1 https://github.com/wHo-EM-i/android_hardware_qcom_media hardware/qcom-caf/msm8998/media -b 8998-4.4
 
-# soc hals
-#rm -rf hardware/qcom-caf/msm8998/display
-#rm -rf hardware/qcom-caf/msm8998/audio
-#rm -rf hardware/qcom-caf/msm8998/media
-#git clone --depth=1 https://github.com/NusantaraProject-ROM/android_hardware_qcom_display hardware/qcom-caf/msm8998/display -b 10-msm8998
-#git clone --depth=1 https://github.com/NusantaraProject-ROM/android_hardware_qcom_audio hardware/qcom-caf/msm8998/audio -b 10-msm8998
-#git clone --depth=1 https://github.com/NusantaraProject-ROM/android_hardware_qcom_media hardware/qcom-caf/msm8998/media -b 10-msm8998
+# nex clang
+git clone --depth=1 https://gitlab.com/Project-Nexus/nexus-clang.git /prebuilts/clang/host/linux-x86/nexus-clang -b nexus-14
 
+cd /bionic
+curl https://github.com/wHo-EM-i/bionic/commit/e8b68f026fb0d6ce13351c55695a6d6cf2742dff.patch | git am
+curl https://github.com/wHo-EM-i/bionic/commit/8bfccafe0a7c5d2584ce0aa414c846a13caee07b.patch | git am
+curl https://github.com/wHo-EM-i/bionic/commit/6db6121472c45316bc6135f05da38227fdc9962e.patch | git am
+
+# patch
+cd /system/libhidl && curl https://github.com/ArrowOS/android_system_libhidl/commit/fbdf10a33f546d202ae12b4b864177fea9faa998.patch | git am
 }
 
 setup () {
+cd /leaf
 . b*/e*.sh
-lunch lineage_lavender-userdebug
+lunch lavender-userdebug
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GMS=true
@@ -53,7 +59,7 @@ ccache -z
 }
 
 build () {
-mka bacon
+m leaf
 }
 
 upload () {
@@ -66,6 +72,6 @@ cd out/target/product/lavender/
 pkg
 sync
 setup
-cache
+#cache
 build
 upload
