33e79ae2b0d2558328935427d513971abbd15044
ImSpiDy <SpiDy2713@gmail.com>
2023-08-14 04:32:33 +0000

    upload files on pixeldrain
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- upload.sh
+++ upload.sh
@@ -5,6 +5,7 @@
 
 echo " "
 echo -e "github release = 1 | devuploads = 2"
+echo -e "pixeldrain = 3"
 read -p "Please enter your number: " UP
 read -p "Please enter file path/name: " FP
 
@@ -14,8 +15,18 @@ FN="$(basename $FP)" && FN="${FN%%.*}"
 echo -e "Started uploading file on github..."
 gh release create $FN --generate-notes --repo $GH
 gh release upload --clobber $FN $FP --repo $GH
-else
+fi
+
+if [ $UP == 2 ]; then
 read -p "Please enter devupload key: " KEY
 echo -e "Started uploading file on DevUploads..."
 bash <(curl -s https://devuploads.com/upload.sh) -f $FP -k $KEY
+fi
+
+if [ $UP == 3 ]; then
+#read -p "Please enter Pixel Drain key: " KEY
+echo -e "Started uploading file on PixelDrain..."
+wget https://github.com/ManuelReschke/go-pd/releases/download/v1.4.0/go-pd_1.4.0_linux_amd64.tar.gz
+tar -xf go-pd*
+./go-pd upload $FP
 fi
