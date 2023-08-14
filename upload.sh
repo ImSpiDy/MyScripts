21f6cc64e34d3867fa8209b869235ab6efaf7927
ImSpiDy <SpiDy2713@gmail.com>
2023-08-14 04:32:33 +0000

    upload files on pixeldrain
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- upload.sh
+++ upload.sh
@@ -5,6 +5,7 @@
 
 echo " "
 echo -e "github release = 1 | devuploads = 2"
+echo -e "pixeldrain = 3"
 read -p "Please enter your number: " UP
 read -p "Please enter file path/name: " FP
 
@@ -14,8 +15,17 @@ FN="$(basename $FP)" && FN="${FN%%.*}"
 echo -e "Started uploading file on github..."
 gh release create $FN --generate-notes --repo $GH
 gh release upload --clobber $FN $FP --repo $GH
-else
+fi
+
+if [ $UP == 2 ]; then
 read -p "Please enter devupload key: " KEY
 echo -e "Started uploading file on DevUploads..."
 bash <(curl -s https://devuploads.com/upload.sh) -f $FP -k $KEY
 fi
+
+if [ $UP == 3 ]; then
+read -p "Please enter Pixel Drain key: " KEY
+echo -e "Started uploading file on PixelDrain..."
+curl -T $FP -u :$KEY https://pixeldrain.com/api/file/
+fi
+
