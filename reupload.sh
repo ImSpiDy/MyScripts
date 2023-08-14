e43721376217930d2ee34eb0669e88165d1a4e82
ImSpiDy <SpiDy2713@gmail.com>
2023-08-14 19:43:05 +0000

    add script to reupload file
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- reupload.sh
+++ reupload.sh
@@ -2,10 +2,13 @@
 #
 # reupload file on pixel drain
 
-wget https://github.com/ChemicalHaxard/BuildOS/releases/download/crdroidandroid/crDroidAndroid-11.0-CORE-20230813-lavender-v7.33.zip
+URL=https://github.com/ChemicalHaxard/BuildOS/releases/download/crdroidandroid/crDroidAndroid-11.0-CORE-20230813-lavender-v7.33.zip
+wget $URL
+
+FileName="$(basename $URL)"
 
 wget https://github.com/ManuelReschke/go-pd/releases/download/v1.4.0/go-pd_1.4.0_linux_amd64.tar.gz
 
 tar -xf go-pd*
 
-./go-pd upload cr*
+./go-pd upload $FileName
