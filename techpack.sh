be9c39d8498dfc7dd8ae1c9f2130ba63787af769
ImSpiDy <SpiDy2713@gmail.com>
2023-09-15 06:26:02 +0530

    techpack: refract script
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- techpack.sh
+++ techpack.sh
@@ -11,7 +11,8 @@ read -p "Please input the tag name: " tag
 read -p " Wanna import audio-kernel? ( y or n ): " ans1
 if [ $ans1 = "y" ]; then
     if [ -d techpack/audio ]; then
-        git subtree pull --prefix=techpack/audio https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel $tag
+        git fetch https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel $tag
+        git merge -X subtree=techpack/audio FETCH_HEAD
     else
         git subtree add --prefix=techpack/audio https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel $tag
     fi
@@ -22,7 +23,8 @@ fi
 read -p " Wanna import data-kernel? ( y or n ): " ans2
 if [ $ans2 = "y" ]; then
     if [ -d techpack/data ]; then
-        git subtree pull --prefix=techpack/data https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/data-kernel $tag
+        git fetch https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/data-kernel $tag
+        git merge -X subtree=techpack/data FETCH_HEAD
     else
         git subtree add --prefix=techpack/data https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/data-kernel $tag
     fi
@@ -33,7 +35,8 @@ fi
 read -p " Wanna import camera-kernel? ( y or n ): " ans3
 if [ $ans3 = "y" ]; then
     if [ -d techpack/camera ]; then
-        git subtree pull --prefix=techpack/camera https://git.codelinaro.org/clo/la/platform/vendor/opensource/camera-kernel/ $tag
+        git fetch https://git.codelinaro.org/clo/la/platform/vendor/opensource/camera-kernel/ $tag
+        git merge -X subtree=techpack/camera FETCH_HEAD
     else
         git subtree add --prefix=techpack/camera https://git.codelinaro.org/clo/la/platform/vendor/opensource/camera-kernel/ $tag
     fi
@@ -44,7 +47,8 @@ fi
 read -p " Wanna import display-drivers? ( y or n ): " ans4
 if [ $ans4 = "y" ]; then
     if [ -d techpack/display ]; then
-        git subtree pull --prefix=techpack/display https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers/ $tag
+        git fetch https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers/ $tag
+        git merge -X subtree=techpack/display FETCH_HEAD
     else
         git subtree add --prefix=techpack/display https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers/ $tag
     fi
@@ -55,7 +59,8 @@ fi
 read -p " Wanna import video-driver? ( y or n ): " ans5
 if [ $ans5 = "y" ]; then
     if [ -d techpack/video ]; then
-        git subtree pull --prefix=techpack/video https://git.codelinaro.org/clo/la/platform/vendor/opensource/video-driver/ $tag
+        git fetch https://git.codelinaro.org/clo/la/platform/vendor/opensource/video-driver/ $tag
+        git merge -X subtree=techpack/video FETCH_HEAD
     else
         git subtree add --prefix=techpack/video https://git.codelinaro.org/clo/la/platform/vendor/opensource/video-driver/ $tag
     fi
